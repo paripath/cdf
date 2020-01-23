f87fd45e3362410a32a0315744054e7559426c72
Rohit Sharma <srohit@gmail.com>
2020-01-23 08:20:38 -0800

    Create cktCommon.h

---
+++ include/cktCommon.h
@@ -0,0 +1,21 @@
+//#############################################################################
+//#                           Copyright Paripath, Inc.
+//#                            All Rights Reserved
+//#############################################################################
+//# Created by: Rohit Sharma
+//#
+//# Revision $Revision: 1.1 $, 
+//# last checked in by $Author: srohit $ on 
+//# $Date: 2015/06/17 14:28:47 $.
+//#
+//# CVS ID: $Id: cktCommon.h,v 1.1 2015/06/17 14:28:47 srohit Exp $
+//#############################################################################
+//#
+//# Description:
+//#   common classes for netlisting etc.
+//
+
+using namespace std;
+
+#define HANNGING_NODE "panglingNode"
+#define HANNGING_BUS  "panglingBus"
